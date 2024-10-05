1571d38673ae2c64e70ea8e393d463b24403d18b
Vicente <adamjon368@gmail.com>
2024-10-05 01:07:11 -0300

    agregados los estilos css, boostrap, y mejorado el css anterior apegandolo más al concepto original, se añadio una carpeta templatetags y se cambiaron algunas cosas del foms para aquello

--- register/forms.py
+++ register/forms.py
@@ -1,6 +1,25 @@
 from django import forms
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(max_length=150, required=True)
-    email = forms.EmailField(required=True)
-    password = forms.CharField(widget=forms.PasswordInput, required=True)
+    username = forms.CharField(
+        max_length=150, 
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',  # Clase de Bootstrap para darle estilo
+            'placeholder': 'Nombre de Usuario'
+        })
+    )
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Correo electrónico'
+        })
+    )
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Contraseña'
+        })
+    )
